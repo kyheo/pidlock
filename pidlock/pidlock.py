63f1bd5e8ff99885ef059f3780f272553dd9aa5a
Martin Marrese <marrese@gmail.com>
2011-09-10 20:42:37 -0300

    Rename file with file_ to avoid any issue ;)

--- pidlock/pidlock.py
+++ pidlock/pidlock.py
@@ -10,11 +10,11 @@ class AlreadyAcquireException(Exception):
 class Pidlock(object):
 
     @classmethod
-    def acquire(cls, file):
+    def acquire(cls, file_):
         locked = True
         #Check if pid_file exists
-        if os.access(file, os.F_OK):
-            fp = open(file, 'r')
+        if os.access(file_, os.F_OK):
+            fp = open(file_, 'r')
             fp.seek(0)
             old_pid = fp.readline()
             fp.close()
@@ -24,17 +24,17 @@ class Pidlock(object):
                 os.remove(file)
         
         if locked:
-            fp = open(file, 'w')
+            fp = open(file_, 'w')
             fp.write("%s" % (os.getpid()))
             fp.close()
-            pl = Pidlock(file)
+            pl = Pidlock(file_)
             return pl
         else:
-            raise AlreadyAcquireException(file)
+            raise AlreadyAcquireException(file_)
 
 
-    def __init__(self, file):
-        self._file = file
+    def __init__(self, file_):
+        self._file = file_
 
 
     def release(self):
